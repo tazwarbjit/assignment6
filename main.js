b5d76b93d456f2957e328d4ba110639b35523934
Tazwar Mohammed Shoumik <tazwar.shoumik@bjitacademy.com>
2023-08-14 16:57:28 +0600

    Second version

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-import users from 'script.js';
+// import users from 'script.js';
 
-const userName = document.getElementById('name');
-const userEmail = document.getElementById('email');
-const userOrganization = document.getElementById('organization');
-const userSession = document.getElementById('session');
+// const userName = document.getElementById('name');
+// const userEmail = document.getElementById('email');
+// const userOrganization = document.getElementById('organization');
+// const userSession = document.getElementById('session');
 
-users = 
+// users = 
 
 $(document).ready(() => {
     $("#registration-form").submit((event) => {
